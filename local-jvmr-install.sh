f89864662bdc55152e9a36170dfb2af36d7a3ff4
Ubuntu <ubuntu@ip-172-31-26-0.eu-west-1.compute.internal>
2014-07-20 14:33:10 +0000

    added script for local installation of jvmr jars as maven artifacts

---
+++ local-jvmr-install.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+lein localrepo install /home/ubuntu/R/x86_64-pc-linux-gnu-library/3.0/jvmr/java/bsh.jar jvmr/bsh "."
+lein localrepo install /home/ubuntu/R/x86_64-pc-linux-gnu-library/3.0/jvmr/java/jline-2.12.jar jvmr/jline "2.12"
+lein localrepo install /home/ubuntu/R/x86_64-pc-linux-gnu-library/3.0/jvmr/java/jvmr_2.11-2.11.1.1.jar jvmr/jvmr "2.11-2.11.1.1"
+lein localrepo install /home/ubuntu/R/x86_64-pc-linux-gnu-library/3.0/jvmr/java/scala-compiler-2.11.1.jar jvmr/scala-compiler "2.11.1"
+lein localrepo install /home/ubuntu/R/x86_64-pc-linux-gnu-library/3.0/jvmr/java/scala-library-2.11.1.jar jvmr/scala-library "2.11.1"
+lein localrepo install /home/ubuntu/R/x86_64-pc-linux-gnu-library/3.0/jvmr/java/scala-reflect-2.11.1.jar jvmr/scala-reflect "2.11.1"
